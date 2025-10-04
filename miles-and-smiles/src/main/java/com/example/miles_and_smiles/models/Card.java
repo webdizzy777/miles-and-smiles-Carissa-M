086925c14d20de0a2a53e5529f485fa490530610
webdizzy777 <carissa.mcmillian@gmail.com>
2025-10-04 15:18:58 -0500

    created Card model

---
+++ miles-and-smiles/src/main/java/com/example/miles_and_smiles/models/Card.java
@@ -0,0 +1,97 @@
+package com.example.miles_and_smiles.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Card {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int card_id;
+
+   private int user_id;
+
+   private String cardName;
+   private LocalDate dateOpened;
+   private double fee;
+   private double apr;
+   private double creditLimit;
+   private double balance;
+   private int dueDay;
+
+   public Card() {
+
+   }
+
+   public Card(String cardName, LocalDate dateOpened, double apr, double fee, double creditLimit, double balance, int dueDay) {
+      this.cardName = cardName;
+      this.dateOpened = dateOpened;
+      this.apr = apr;
+      this.fee = fee;
+      this.creditLimit = creditLimit;
+      this.balance = balance;
+      this.dueDay = dueDay;
+   }
+
+   public String getCardName() {
+      return cardName;
+   }
+
+   public void setCardName(String cardName) {
+      this.cardName = cardName;
+   }
+
+   public LocalDate getDateOpened() {
+      return dateOpened;
+   }
+
+   public void setDateOpened(LocalDate dateOpened) {
+      this.dateOpened = dateOpened;
+   }
+
+   public double getFee() {
+      return fee;
+   }
+
+   public void setFee(double fee) {
+      this.fee = fee;
+   }
+
+   public double getApr() {
+      return apr;
+   }
+
+   public void setApr(double apr) {
+      this.apr = apr;
+   }
+
+   public double getCreditLimit() {
+      return creditLimit;
+   }
+
+   public void setCreditLimit(double creditLimit) {
+      this.creditLimit = creditLimit;
+   }
+
+   public double getBalance() {
+      return balance;
+   }
+
+   public void setBalance(double balance) {
+      this.balance = balance;
+   }
+
+   public int getDueDay() {
+      return dueDay;
+   }
+
+   public void setDueDay(int dueDay) {
+      this.dueDay = dueDay;
+   }
+
+}
