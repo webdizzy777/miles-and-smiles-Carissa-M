f06c08842bf761641e4a5423b086fa5aff96987b
webdizzy777 <carissa.mcmillian@gmail.com>
2025-10-04 15:31:13 -0500

    Created User model

---
+++ miles-and-smiles/src/main/java/com/example/miles_and_smiles/models/User.java
@@ -0,0 +1,73 @@
+package com.example.miles_and_smiles.models;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    public User(){
+
+    }
+    
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    //PrePersist will run automatically before hibernate inserts a row into the table
+    //Use it to set createdAt to the current time for the timestamp
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+}
