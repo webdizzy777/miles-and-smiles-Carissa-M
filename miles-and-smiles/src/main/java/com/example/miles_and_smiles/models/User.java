420eae02cdf0f1e1325791c3654b6860d843a238
webdizzy777 <carissa.mcmillian@gmail.com>
2025-10-04 15:33:17 -0500

    updated application properties

--- miles-and-smiles/src/main/java/com/example/miles_and_smiles/models/User.java
+++ miles-and-smiles/src/main/java/com/example/miles_and_smiles/models/User.java
@@ -19,7 +19,7 @@ public class User {
     public User(){
 
     }
-    
+
     public User(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
