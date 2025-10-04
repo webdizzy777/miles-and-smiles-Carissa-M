acadee30cdfd5a64b72b3f08b591dfc1c638fe25
webdizzy777 <carissa.mcmillian@gmail.com>
2025-10-04 15:51:01 -0500

    created Expiring Reward Model

--- miles-and-smiles/src/main/java/com/example/miles_and_smiles/models/ExpiringReward.java
+++ miles-and-smiles/src/main/java/com/example/miles_and_smiles/models/ExpiringReward.java
@@ -2,8 +2,67 @@ package com.example.miles_and_smiles.models;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 public class ExpiringReward {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reward_id;
+
+    @ManyToOne
+    @JoinColumn(name = "card_id", nullable = false)
+    private Card card;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+    @Column(nullable = false)
+    private LocalDate expirationDate;
+
+    private String details;
+
+    public ExpiringReward(){
+
+    }
+
+    public ExpiringReward(String title, String details, LocalDate expirationDate, Card card) {
+        this.title = title;
+        this.details = details;
+        this.expirationDate = expirationDate;
+        this.card = card;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 
 }
