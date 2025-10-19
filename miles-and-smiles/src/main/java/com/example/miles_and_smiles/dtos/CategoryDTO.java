585781093648b55f681572347c0f8e6283b13cb1
webdizzy777 <carissa.mcmillian@gmail.com>
2025-10-19 13:55:34 -0500

    created categoryDTO

--- miles-and-smiles/src/main/java/com/example/miles_and_smiles/dtos/CategoryDTO.java
+++ miles-and-smiles/src/main/java/com/example/miles_and_smiles/dtos/CategoryDTO.java
@@ -1,4 +1,29 @@
 package com.example.miles_and_smiles.dtos;
 
 public class CategoryDTO {
+    private int categoryId;
+    private String categoryName;
+
+    public CategoryDTO() {}
+
+    public CategoryDTO(int categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
